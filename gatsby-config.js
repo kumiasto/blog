bad447abfcb38b5a1946dce23b0b6cef9fbafeb1
Kamil Umiastowski <kumiasto@gmail.com>
2021-04-06 21:09:57 +0200

    add styled components plugin into gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-transformer-remark`,
       options: {
